a7fb7c84592674c990ee5c482accb1f89606db9c
elenapanasyuk <elena.panasyuk@gmail.com>
2021-01-25 23:11:04 +0200

    completed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import 'modern-normalize/modern-normalize.css';
 import './index.css';
 import App from './App';
+import { store } from './redux/store';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
